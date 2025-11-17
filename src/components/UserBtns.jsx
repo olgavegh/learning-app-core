52799616d8e4bb0d2fc7582f813b142502b565b1
pz <pasztor.zzz@gmail.com>
2025-11-17 11:43:04 +0100

    add_UserBtns.jsx_only_displaying_Sign-up-in-out_buttons_yet_without_functionality_and_ternary_logic

---
+++ src/components/UserBtns.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function UserBtns() {
+    
+    // --- Button logic: signed out -> Sign up & Sign out : signed in -> "Sign out"
+    return (
+        <>
+            <button>Sign up</button>
+            <button>Sign in</button>
+            <button>Sign out</button>
+        </>
+    )
+}
+
+export default UserBtns;
