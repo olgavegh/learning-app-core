8c97409aee6cd1a604a08ba1160ff3a60f8fc56d
olgavegh <olgi.vegh@gmail.com>
2025-11-17 12:54:50 +0100

    add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css'
+
+import { Routes, Route, Link } from "react-router-dom"
+import { getNavItems } from "./constants/navigationData";
+
 import usePageNavigation from './hooks/usePageNavigation';
 import BookmarkedPage from './pages/BookmarkedPage';
 import StudyPage from "./pages/StudyPage";
@@ -13,14 +17,42 @@ function App() {
 
   return (
     <>
-      <Header currentPage={currentPage} navigateToPage={navigateToPage} />
-      <div className="">
-        {currentPage === "dashboard" && <DashBoard navigateToPage={navigateToPage} />}
-        {currentPage === "study" && <StudyPage />}
-        {currentPage === "matching" && <MatchingPage />}
-        {currentPage === "test" && <TestPage />}
-        {currentPage === "bookmarks" && <BookmarkedPage />}
-      </div>
+      <header>
+        <nav className="navbar navbar-expand-lg sticky-top">
+          <div className="container-fluid">
+            <Link to="/" className="navbar-brand" >Codey</Link>
+
+
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse align-items-center" id="navbarNav">
+
+              {getNavItems(currentPage).map((page, index) => (
+                <Link to={page.id} className="nav-item fw-semibold">{page.text}</Link>
+              ))}
+              <div>
+                <button className="navbar-cta" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="cta" aria-expanded="false" aria-label="Call to Action">
+                  Call Barna
+                </button>
+              </div>
+
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <Routes>
+        {/* <div className="">
+          {currentPage === "dashboard" && <DashBoard navigateToPage={navigateToPage} />}
+          {currentPage === "study" && <StudyPage />}
+          {currentPage === "matching" && <MatchingPage />}
+          {currentPage === "test" && <TestPage />}
+          {currentPage === "bookmarks" && <BookmarkedPage />}
+        </div> */}
+
+      </Routes>
       <Footer />
     </>
   )
