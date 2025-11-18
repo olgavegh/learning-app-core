c3eda4c88bdb9981d544169852ec1aa1d96cb1d6
pz <pasztor.zzz@gmail.com>
2025-11-18 12:15:58 +0100

    add_sign-up-in-out_functionalities_using_'handle...'_functions_and_hardcoded_user_still

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,79 @@
 import './App.css'
 
-import { Routes, Route, Link } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom";
+import { auth } from './firebase';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut
+} from 'firebase/auth';
 import { getNavItems } from "./constants/navigationData";
+import { useState, useEffect } from 'react';
 import UserBtns from "./components/UserBtns";
 import DashBoard from "./pages/DashBoard";
 import Footer from "./components/Footer";
 
+const TEST_EMAIL = 'test@example.com';
+const TEST_PASSWORD = 'testPassword';
 
 function App() {
+  const [message, setMessage] = useState(null);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      console.log('The user\'s auth state changed:', currentUser);
+      setUser(currentUser);
+    })
+  }, [])
+
+  async function handleSignUp() {
+    setError(null);
+    setMessage(null);
+
+    try {
+      const registeredUser = await createUserWithEmailAndPassword(auth, TEST_EMAIL, TEST_PASSWORD);
+      console.log('S-up successfull');
+      console.log(registeredUser);
+      setMessage('✅ Sign-up successful!');
+    } catch (error) {
+      console.log('S-up failed', error);
+      setError(error.message)
+    }
+  }
+
+  async function handleSignIn() {
+    setError(null);
+    setMessage(null);
+
+    try {
+      const loggedInUser = await signInWithEmailAndPassword(auth, TEST_EMAIL, TEST_PASSWORD);
+      console.log('S-in successfull');
+      console.log(loggedInUser);
+      setMessage('✅ Sign-in successful!');
+    } catch (error) {
+      console.log('S-in failed', error);
+      setError(error.message)
+    }
+  }
+
+  async function handleSignOut() {
+    setError(null);
+    setMessage(null);
+
+    try {
+      const signedOut = await signOut(auth);
+      console.log('S-out successfull');
+      console.log(signedOut);
+      setMessage('✅ Sign-out successful!');
+    } catch (error) {
+      console.log('sign-out failed', error);
+      setError(error.message);
+    }
+  }
+
 
   return (
     <>
@@ -22,7 +88,11 @@ function App() {
 
             <div className="collapse navbar-collapse" id="navbarNav">
               <div className="d-flex align-items-center me-auto">
-                <UserBtns />
+                <UserBtns
+                  onSignUp={handleSignUp}
+                  onSignIn={handleSignIn}
+                  onSignOut={handleSignOut}
+                />
               </div>
               <ul className="navbar-nav ms-auto align-items-center">
                 {getNavItems().map((page, index) => (
@@ -40,6 +110,12 @@ function App() {
           </div>
         </nav>
       </header>
+
+      {/*<div>
+        {user ? (<div>{user.email} is signed in</div>) : (<div>No user signed in</div>)}
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+        {message && <div style={{ color: 'green' }}>{message}</div>}
+      </div>*/}
 
       <Routes>
         <Route path="/" element={<DashBoard />} />
