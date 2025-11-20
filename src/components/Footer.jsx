553ab6779970c02c45ce39ffffd5dccf50906d1d
olgavegh <olgi.vegh@gmail.com>
2025-11-20 01:40:00 +0100

    fix layout flex behavior - fix footer position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 function Footer() {
     return (
-        <div className="footer">
-            <h5>Thanks for visiting our page! Have a nice day! ;)</h5>
-            <h6>Developers: Olga Végh, Tamás Szondi, Zoltán Pásztor</h6>
-        </div>
+        <footer className="footer mt-auto">
+            <div className="container">
+                <div className="row align-items-center justify-content-between">
+                    <div className="col-md-6">
+                        <h5 className="footer-brand mb-2">CODEY</h5>
+                        <p className="footer-text mb-0">Thanks for visiting our page! Have a nice day! 😊</p>
+                    </div>
+                    <div className="col-md-6 text-md-end">
+                        <p className="footer-copyright mb-1">© 2025 Codecool Assessment Practice. All rights reserved.</p>
+                        <p className="footer-developers mb-0">Developers: Olga Végh, Tamás Szondi, Zoltán Pásztor</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
