efbf8226959d249245c75a068fecaf345f517d81
olgavegh <olgi.vegh@gmail.com>
2025-11-20 16:34:28 +0100

    refresh bookmark page

--- src/apis/bookmarkQuestion.api.js
+++ src/apis/bookmarkQuestion.api.js
@@ -1,17 +1,23 @@
+import { auth } from "../firebase";
+
 export default async function updateQuestion(id, bookmark) {
-    const url = "https://learning-supp-fb-app-tm-notyet-default-rtdb.europe-west1.firebasedatabase.app/questions/" + id + ".json";
+  const currentUser = auth.currentUser;
+  const token = currentUser ? await currentUser.getIdToken() : null;
+  const url = `https://learning-supp-fb-app-tm-notyet-default-rtdb.europe-west1.firebasedatabase.app/questions/${id}.json${
+    token ? `?auth=${token}` : ""
+  }`;
 
-    const response = await fetch(url, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ bookmark }),
-    })
+  const response = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ bookmark }),
+  });
 
-    if (!response.ok) {
-      throw new Error("Update question failed");
-    }
-
-    return response;
+  if (!response.ok) {
+    throw new Error("Update question failed");
   }
+
+  return response;
+}
--- src/pages/BookmarkedPage.jsx
+++ src/pages/BookmarkedPage.jsx
@@ -1,4 +1,5 @@
 import QuestionList from "../components/QuestionList";
+import TitleContainer from "../components/layout/TitleContainer"
 import useBookmarkedQuestions from "../hooks/useBookmarkedQuestions"
 
 export default function BookmarkedPage() {
@@ -6,7 +7,12 @@ export default function BookmarkedPage() {
 
   return (
     <>
-    <QuestionList questions={questions} loading={loading} error={error} errorMessage={errorMessage} setQuestions={setQuestions}></QuestionList>
+      <TitleContainer
+        badgeText="Bookmarks"
+        titleText="Your Bookmarked Questions"
+        descText="Quickly access all the questions you’ve marked for review. Use this page to revisit, study, and manage your favorite or most challenging questions."
+      />
+      <QuestionList useAccordion={true} questions={questions} loading={loading} error={error} errorMessage={errorMessage} setQuestions={setQuestions}></QuestionList>
     </>
   )
 }
