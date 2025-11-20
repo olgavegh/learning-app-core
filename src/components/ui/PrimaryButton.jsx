b9688bf0102be6103a3a1fbc49f17ce5f88b7bb1
olgavegh <olgi.vegh@gmail.com>
2025-11-20 15:37:59 +0100

    some layot styling

--- src/components/ui/PrimaryButton.jsx
+++ src/components/ui/PrimaryButton.jsx
@@ -10,10 +10,10 @@ export default function PrimaryButton({
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={widthCss + " btn bg-primary border border-2 border-dark text-dark fw-semibold text-center text-nowrap px-4 py-2 neo-shadow"}
+            className={widthCss + " btn bg-primary border border-2 border-dark text-dark fw-medium text-center text-nowrap px-4 py-2 neo-shadow"}
             style={{
                 fontFamily: 'Epilogue, "Epilogue Placeholder", sans-serif',
-                fontSize: '14px',
+                fontSize: '1rem',
                 letterSpacing: '-0.04em',
                 lineHeight: '1.5em',
                 transition: 'all 0.15s ease'
